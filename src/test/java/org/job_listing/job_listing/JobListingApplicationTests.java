39355c39647fcd0a31c1f68a39bf37d97f8fe017
Harshith N J <njharshith122@gmail.com>
2025-03-24 22:34:51 +0530

    project created

---
+++ src/test/java/org/job_listing/job_listing/JobListingApplicationTests.java
@@ -0,0 +1,13 @@
+package org.job_listing.job_listing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobListingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
