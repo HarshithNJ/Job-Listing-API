c9a3730f2e1422846118d7a39087dd963248dc3a
Harshith N J <njharshith122@gmail.com>
2025-03-26 00:13:02 +0530

    post a job

--- src/main/java/org/job_listing/job_listing/controller/jobController.java
+++ src/main/java/org/job_listing/job_listing/controller/jobController.java
@@ -1,7 +1,11 @@
 package org.job_listing.job_listing.controller;
 
+import org.job_listing.job_listing.dto.job;
 import org.job_listing.job_listing.service.jobService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -9,4 +13,11 @@ public class jobController {
     
     @Autowired
     jobService service;
+
+
+    //To Post the Job Listing
+    @PostMapping("/jobs")
+    public ResponseEntity<Object> postJob(@RequestBody job job) {
+        return service.postJob(job);
+    }
 }
--- src/main/java/org/job_listing/job_listing/service/jobService.java
+++ src/main/java/org/job_listing/job_listing/service/jobService.java
@@ -1,7 +1,13 @@
 package org.job_listing.job_listing.service;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.job_listing.job_listing.dto.job;
 import org.job_listing.job_listing.repository.jobRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -9,4 +15,21 @@ public class jobService {
     
     @Autowired
     jobRepository repository;
+
+    public ResponseEntity<Object> postJob(job job) {
+        if(repository.existsById(job.getId())){
+            Map<String, Object> map = new HashMap<String, Object>();
+            map.put("message", "Job already exists");
+
+            return new ResponseEntity<Object>(map, HttpStatus.BAD_REQUEST);
+        }else{
+            repository.save(job);
+
+            Map<String, Object> map = new HashMap<String, Object>();
+            map.put("message", "Job posted successfully");
+            map.put("job", job);
+
+            return new ResponseEntity<Object>(map, HttpStatus.CREATED);
+        }
+    }
 }
